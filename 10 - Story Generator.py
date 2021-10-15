761bfc17970e4eda3f823d037abe99cd10e757bb
Raul Sanchez <77367918+SanchezProgramming@users.noreply.github.com>
2021-10-15 11:03:03 -0500

    Add files via upload

---
+++ 10 - Story Generator.py
@@ -0,0 +1,9 @@
+
+print("Hello my name is _____, I am going to the ______ for the day.\nAfterwards I would like to eat _____.\n")
+
+
+name = input("Enter name: ")
+place = input("Enter place: ")
+food = input("Enter food to eat: ")
+
+print(f"\nHello my name is {name}, I am going to the {place} for the day.\nAfterwards I would like to eat {food}.")
